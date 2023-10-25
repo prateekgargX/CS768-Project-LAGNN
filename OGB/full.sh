916169e2fb761c0492fb2dae02a542489f82689a
Prateek Garg <84305275+prateekgargX@users.noreply.github.com>
2023-10-25 18:27:47 +0530

    fix lagat commands in full.sh
    issue with naming of some arguments fixed

--- OGB/full.sh
+++ OGB/full.sh
@@ -6,5 +6,5 @@ python lasage_arxiv.py --concat 1 --samples 4 --num_layers 3 --hidden_channels 1
 python lasage_proteins.py --concat 1 --samples 2 --num_layers 3 --hidden_channels 128 --lr 0.01 --epochs 1000
 python lasage_products_average.py --concat 1 --samples 4 --hidden_channels 256 --tem 0.5 --lam 1
 
-python lagat_arxiv.py --concat 1 --samples 2 --hidden_channels 125 --num_layers 3 --lam 1 --tem 0.5 --epochs 2000 --use-norm --use-labels --no-attn-dst --edge-drop 0 --input-drop 0
+python lagat_arxiv.py --concat 1 --samples 2 --n-hidden 125 --n-layers 3 --lam 1 --tem 0.5 --n-epochs 2000 --use-norm --use-labels --no-attn-dst --edge-drop 0 --input-drop 0
 python lagat_products_average.py --concat 1 --samples 2
